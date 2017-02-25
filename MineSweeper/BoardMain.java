0856e26ec069de696d2ebfc6ff7b914ce97f3c67
ryanthnguyen <qtquoc@memphis.edu>
2017-02-25 12:53:07 -0600

    Fifth Commit

--- MineSweeper/BoardMain.java
+++ MineSweeper/BoardMain.java
@@ -6,15 +6,20 @@ public class BoardMain
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        System.out.print("row dimension? ");
+        System.out.print("Row dimension: ");
         int row = input.nextInt();
-        System.out.print("Column dimension? ");
+        System.out.print("Column dimension: ");
         int column = input.nextInt();
-        GameBoard mainBoard = new GameBoard(row,column);
-        //need to go to the loop in order to return the board\
-         for (int i = 0; i < 1;i++)
-         {
-         mainBoard.getBoard();
+        System.out.print("Amount of Mines: ");
+      	int numMines = input.nextInt();
+        GameBoard mainBoard = new GameBoard(numMines,row,column);
+        /*need to go to the loop in order to return the board\
+      	gotta keep game going my man until you hit a mine 
+        either do loop or while loop 
+        */
+        for (int i = 0; i < 1;i++)
+        {
+            mainBoard.getBoard();
         }
     }
 }
--- MineSweeper/GameBoard.java
+++ MineSweeper/GameBoard.java
@@ -1,32 +1,52 @@
-//Ryan Nguyen
-//Making the board
 import java.util.*;
 public class GameBoard
 {
+  private int mines;
   private int rows;
   private int columns;
-  private Tile[][] board;
+  private Tile board = new Tile();;
+  private String[][] gameBoard;
   private int[][] positionOfMines;
-  public GameBoard(int row, int column)
+  public GameBoard(int mine,int row, int column)
   {
     //instantiating the board
+    mines = mine;
     rows = row;
     columns = column;
-    board = new Tile[row][column];
+    gameBoard = new String[rows][columns];
+    positionOfMines = new int[mines][2];
   }
-  public void distributeMines(int mines)
+  public void getBoard()
   {
-    int x = 0;
+      for (int i = 0; i < gameBoard.length;i++)
+      {
+          for (int j = 0; j < gameBoard[i].length;j++)
+          {
+              System.out.print(gameBoard[i][j] = board.getCoveredTile());
+          }
+          System.out.println();
+      }
+  }
+  public void distributeMines()
+  {
     //this will distributes the mines in the 2d array
-    int numberOfMines = mines / 2;
     //stores the location of the mines in anothe 2d array;
-    for (int i = 0; i < rows;i++)
+    for (int i = 0; i < mines;i++)
     {
       for (int j = 0; j < 2;j++)
-      int randomRows = (int) (rows * Math.random());
-      int randomColumns = (int) (columns * Math.random());
-      positionOfMines[i][j] = (int)(numberOfMines * Math.random());
-      }
+    {
+        positionOfMines[i][j] = (int)(mines * Math.random());
+        for (int k = 0;k < mines;k++)
+        {
+            for (int l = 0; l < 2;l++)
+            {
+                gameBoard[positionOfMines[i][j]][positionOfMines[i][j]] = board.getBombTile();
+            }
+        }
+    }
     }
   }
 }
+
+
+
